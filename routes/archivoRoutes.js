cf07ed887524efb02bfa504915a4bc716505b6aa
DyllanRuiz <dyllan.ruiz.vasquez@est.una.ac.cr>
2023-10-10 16:51:30 -0600

    Protecion de ruta Archivo

--- routes/archivoRoutes.js
+++ routes/archivoRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import checkRole from '../middlewares/roleMiddleware.js';
 import { subirArchivo, obtenerArchivo, obtenerArchivos, eliminarArchivo } from '../controllers/archivoController.js';
+
 
 const archivoRouter = express.Router();
 
-archivoRouter.post('/archivo', subirArchivo);
-archivoRouter.get('/archivo/:nombreArchivo', obtenerArchivo);
-archivoRouter.get('/archivo', obtenerArchivos);
-archivoRouter.delete('/archivo/:nombreArchivo', eliminarArchivo);
+archivoRouter.post('/archivo',checkRole(['Director', 'Maestra']), subirArchivo);
+archivoRouter.get('/archivo/:nombreArchivo',checkRole(['Director', 'Maestra', 'Estudiante']), obtenerArchivo);
+archivoRouter.get('/archivo', checkRole(['Director', 'Maestra', 'Estudiante']),obtenerArchivos);
+archivoRouter.delete('/archivo/:nombreArchivo', checkRole(['Director']),eliminarArchivo);
 
 
 export default archivoRouter;
---
+++ services/archivoServices.js
@@ -0,0 +1,64 @@
+import archivoRepository from "../repositories/archivo.Repository.js";
+
+const archivoService = {
+
+    subirArchivo: async (archivo) => {
+        try {
+            const nuevoArchivo = await archivoRepository.crear(archivo);
+            return nuevoArchivo;
+        } catch (error) {
+            const errors = [];
+            if (error.name === "SequelizeValidationError") {
+                error.errors.forEach((e) => {
+                    errors.push({
+                        Type: "Validation Error",
+                        field: e.path,
+                        message: e.message,
+                    });
+                });
+            } else {
+                errors.push({ field: "general", message: error.message });
+            }
+            throw errors;
+        }
+    },
+
+    obtenerArchivos: async () => {
+        try {
+            const archivos = await archivoRepository.obtenerTodos();
+            return archivos;
+        } catch (errors) {
+            errors.push({ field: "general", message: error.message });
+            throw errors;
+        }
+
+    },
+
+    obtenerArchivoPorId: async (id) => {
+        try {
+            const archivo = await archivoRepository.obtenerPorId(id);
+            if (!archivo) {
+                throw new Error("Archivo no encontrado");
+            }
+            return archivo;
+        } catch (error) {
+            throw error
+        }
+    },
+
+
+    borrarArchivo: async (id) => {
+        try {
+            const archivo = await archivoRepository.borrar(id);
+            if (!archivo) {
+                throw new Error("Archivo no encontrado");
+            }
+            return archivo;
+        } catch (error) {
+            throw error
+        }
+    },
+
+};
+
+export default archivoService;
