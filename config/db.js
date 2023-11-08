b1c9552ffd5d3a18f01a89f414018c9f8f5f9e70
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2023-11-08 10:09:11 -0600

    Refactor database connection configuration

--- config/db.js
+++ config/db.js
@@ -1,22 +1,21 @@
 import { Sequelize } from "sequelize";
 
 // String de conexión a la base de datos
-const connectionString = "Server=MYSQL5045.site4now.net;Database=db_aa134c_awaegdb;Uid=aa134c_awaegdb;Pwd=wwsw22u0";
+//const connectionString = "host=MYSQL5045.site4now.net;database=db_aa134c_awaegdb;user=aa134c_awaegdb;password=wwsw22u0";
 
 // Configuración de la DB y conexión
-const db = new Sequelize(connectionString, {
-  dialect: "mysql",
-  timezone: "-06:00",
-  logging: false,
-  define: {
-    timestamps: true,
-  },
-  pool: {
-    max: 5, // Conexiones activas a mantener
-    min: 0, // Mínimas conexiones
-    acquire: 3000, // Tiempo antes de marcar error de conexión (3000 = 30s)
-    idle: 10000, // Tiempo antes de cerrar las conexiones si no hay movimiento (10000 = 10s)
-  },
-});
+const db = new Sequelize({
+    dialect: 'mysql',
+    host: 'MYSQL5045.site4now.net',
+    database: 'db_aa134c_awaegdb',
+    username: 'aa134c_awaegdb',
+    password: 'wwsw22u0',
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  });
 
 export default db;
