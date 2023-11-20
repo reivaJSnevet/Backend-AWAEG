e11769d68de624e8a3ec76df4792519d37c0187a
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2023-11-20 16:09:41 -0600

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
    "https://localhost:7077",
-   "https://frontend-awaeg.vercel.app",
+   "https://awaeg.vercel.app",
    
 ];
 
