f4bd95de3e19a826aae3997feb0692cdb8fbd647
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2024-01-16 01:04:02 -0600

    Refactor seeder.js for improved readability and error handling

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,37 +1,48 @@
 import db from "../config/db.js";
-import { Role, User, Student, Caregiver, Group, Functionary } from "../models/index.js";
-import { roles, users, students, caregivers, groups, functionaries } from "./index.js";
+import {
+	Role,
+	User,
+	Student,
+	Caregiver,
+	Group,
+	Functionary,
+} from "../models/index.js";
+import {
+	roles,
+	users,
+	students,
+	caregivers,
+	groups,
+	functionaries,
+} from "./index.js";
 
 const importarDatos = async () => {
-	/* const transaction = await db.transaction(); */
-
 	try {
 		await db.authenticate();
 		await db.sync({ force: true });
 
-		await Role.bulkCreate(roles/* , { transaction } */);
-		await User.bulkCreate(users/* , { transaction } */);
+		await Role.bulkCreate(roles);
+		await User.bulkCreate(users);
 
-        await Functionary.bulkCreate(functionaries/* , { transaction } */);
-        await Group.bulkCreate(groups/* , { transaction } */);
-        
-        await Caregiver.bulkCreate(caregivers/* , { transaction } */);
-        await Student.bulkCreate(students/* , { transaction } */);
+		await Functionary.bulkCreate(functionaries);
+		await Group.bulkCreate(groups);
+
+		await Caregiver.bulkCreate(caregivers);
+		await Student.bulkCreate(students);
 
 		console.log(
+			"\x1b[36m%s\x1b[0m",
 			"A skilled seeder orchestrated a symphony of data growth, it planted the seeds of innovation, nurturing a flourishing garden of progress from a barren database.",
 		);
 
-       /*  await transaction.commit(); */
 		process.exit(0);
 	} catch (error) {
 		console.log(
+			"\x1b[31m%s\x1b[0m",
 			"Unfortunately, the seeder encountered a glitch, leaving the database barren and the seeds of progress unattended.",
-			"ERROR: ",
-			error,
 		);
+		console.log("ERROR: ", error);
 
-		/* await transaction.rollback(); */
 		process.exit(1);
 	}
 };
